e8b3118dba058352d422be00fd6c6dbabf77de44
Nikita Gryzlov <nixel2007@gmail.com>
2016-02-29 16:15:38 +0300

    Добавлена настройка "lintBSLFiles"

--- linter.py
+++ linter.py
@@ -5,8 +5,12 @@ from SublimeLinter.lint import Linter, util
 class OneScriptLint(Linter):
     """Provides an interface to OneScriptLint."""
 
+    lintBSLFiles = sublime.load_settings('1c.sublime-settings').get('lintBSLFiles');
+    file_name = sublime.active_window().active_view().file_name();
+
     syntax = '1c'
-    cmd = ('oscript -encoding=utf-8 -check @')
-    regex = r'\{\S+\s+(?P<error>.*)\s\/\s.*:\s+(?P<line>\d+)\s+\/\s+(?P<message>.*)\}'
-    error_stream = util.STREAM_BOTH
-    comment_re = r'\s*//'
+    if not (file_name.endswith('.bsl') and not lintBSLFiles):
+	    cmd = ('oscript -encoding=utf-8 -check @')
+	    regex = r'\{\S+\s+(?P<error>.*)\s\/\s.*:\s+(?P<line>\d+)\s+\/\s+(?P<message>.*)\}'
+	    error_stream = util.STREAM_BOTH
+	    comment_re = r'\s*//'
