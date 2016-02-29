342067a93262256e57f70da468ff37716d41b0a6
Nikita Gryzlov <nixel2007@gmail.com>
2016-02-29 19:07:05 +0300

    Исправлен учет свойства

--- linter.py
+++ linter.py
@@ -6,12 +6,13 @@ from SublimeLinter.lint import Linter, util
 class OneScriptLint(Linter):
     """Provides an interface to OneScriptLint."""
 
-    lintBSLFiles = sublime.load_settings('1c.sublime-settings').get('lintBSLFiles');
-    file_name = sublime.active_window().active_view().file_name();
-
     syntax = '1c'
-    if not (file_name.endswith('.bsl') and not lintBSLFiles):
-	    cmd = ('oscript -encoding=utf-8 -check @')
-	    regex = r'\{\S+\s+(?P<error>.*)\s\/\s.*:\s+(?P<line>\d+)\s+\/\s+(?P<message>.*)\}'
-	    error_stream = util.STREAM_BOTH
-	    comment_re = r'\s*//'
+    cmd = ('oscript -encoding=utf-8 -check @')
+    regex = r'\{\S+\s+(?P<error>.*)\s\/\s.*:\s+(?P<line>\d+)\s+\/\s+(?P<message>.*)\}'
+    error_stream = util.STREAM_BOTH
+    comment_re = r'\s*//'
+
+    def run(self, cmd, code):
+        lintBSLFiles = sublime.load_settings('Language 1C (BSL).sublime-settings').get('lintBSLFiles')
+        if not (self.filename.endswith('.bsl') and not lintBSLFiles):
+            return self.communicate(cmd, code)
